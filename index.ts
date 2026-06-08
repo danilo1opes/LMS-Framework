0c04781eef660be9ba0701b5385927e71cf146ed
danilo1opes <parceriasnyx@gmail.com>
2026-06-08 11:15:46 -0300

    🏗️  feat: Create new tables SQLite, start building

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import { Core } from './core/core.ts';
 import { logger } from './core/middleware/logger.ts';
-import { ProductsApi } from './api/produtcs/index.ts';
+import { AuthApi } from './api/auth/index.ts';
+import { LmsApi } from './api/lms/index.ts';
 
 const core = new Core();
 
 core.router.use([logger]);
 
-new ProductsApi(core).init();
+new AuthApi(core).init();
+new LmsApi(core).init();
 
 core.router.get('/', (req, res) => {
   res.status(200).json('ola');
